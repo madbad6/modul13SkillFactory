7a4e8c54f4716223575379a4a506ea4e8e01c0fc
Nicolay Elsukov <madbad6@yandex.ru>
2021-10-26 11:30:47 +0300

    synchronized brunch

--- src/com/madbad/CountThread.java
+++ src/com/madbad/CountThread.java
@@ -4,10 +4,12 @@ public class CountThread implements Runnable{
     int count;
     @Override
     public void run() {
-        count = 0;
-        for (int i = 0; i < 3; i++){
-            count++;
-            System.out.println(Thread.currentThread().getName() +": "+ count);
+        synchronized (this) {
+            count = 0;
+            for (int i = 0; i < 3; i++) {
+                count++;
+                System.out.println(Thread.currentThread().getName() + ": " + count);
+            }
         }
     }
 }
